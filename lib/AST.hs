62b5126c3c47380f5993ac94b80c4e400501456c
kakkak444 <172775100+kakkak444@users.noreply.github.com>
2025-02-09 19:44:31 +0900

    implement AST

--- lib/AST.hs
+++ lib/AST.hs
@@ -1,3 +1,11 @@
 module AST
-    (
+    ( Ast(..)
+    , Literal(..)
     ) where
+
+import AST.Types
+
+data Ast
+    = Lit Literal
+    | Add Ast Ast | Sub Ast Ast | Mul Ast Ast | Div Ast Ast
+    deriving (Show)
--- lib/AST/Types.hs
+++ lib/AST/Types.hs
@@ -1,3 +1,12 @@
 module AST.Types
-    (
+    ( Literal(..)
+    , SomeNumber(..)
+    , Number(..)
+    , NumType(..)
     ) where
+
+import AST.Types.Number
+
+data Literal
+    = Num SomeNumber
+    deriving (Show)
